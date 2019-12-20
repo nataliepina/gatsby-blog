82c8582f9be5f1e2de911267dc0fd00b9831c5e8
Natalie Pina <natalie.a.pina@gmail.com>
2019-12-20 14:51:41 -0500

    add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`],
+  siteMetadata:{
+    title: 'blog',
+    description: 'blog description',
+    author: '@nataliepina',
+    data:['item1', 'item2'],
+  },
+  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
